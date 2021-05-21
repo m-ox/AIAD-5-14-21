38c6d8f4f8c299868e3dd700162b104871bb31e6
c-masque <maudlin.oxalis@gmail.com>
2021-05-21 13:17:50 -0600

    Added a simple get request on new note page.

--- src/components/pages/new.js
+++ src/components/pages/new.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
 
@@ -7,14 +8,38 @@ export default class App extends Component {
 
         this.mocky = require('../mockData')
         console.log(this.mocky.mockData[0].content) //id
+
+        this.state = {
+            stickies: []
+        }
+    }
+
+    getStickies() {
+        axios.get("https://spacecamp.devcamp.space/portfolio/portfolio_blogs", {withCredentials: true})
+            .then(response => {
+                console.log("sticky response", response)
+            })
+            .catch(error => {
+                console.log("error", error)
+            })
+    }
+
+    componentDidMount() {
+        this.getStickies()
     }
   
     render() {
         // RIP OFF VIEW PAGE, JUST MAKE IT EMPTY
-      return (
-          <div>
-              {`${this.mocky.mockData[0].title}`}
-          </div>
-      );
+        return (
+            <div className="container">
+              <div className="view-container">
+                {`${this.mocky.mockData[0].title} created by ${this.mocky.mockData[0].user}`}
+                <textarea>
+                  
+                </textarea>
+              </div>
+            </div>
+        );
+      }
     }
-  }
+    
